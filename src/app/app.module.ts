a83c086127a3c5177a64e7880027ae3a8e262322
peter-cbxu <cbxu@uwaterloo.ca>
2017-05-14 16:29:59 -0400

    Set up routing module and reorganize directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-component/app.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
