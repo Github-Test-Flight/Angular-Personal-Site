140c29f21cc95db861dc7f781d1573e9d84c4a29
peter-cbxu <cbxu@uwaterloo.ca>
2017-05-17 17:41:01 -0400

    Finish styling mobile

--- src/app/data/experience_data.ts
+++ src/app/data/experience_data.ts
@@ -19,7 +19,7 @@ export const EXPERIENCES: Experience[] = [
             '/assets/td/2.jpg',
             '/assets/td/3.jpg',
         ],
-        skills: 'Angular, TypeScript, ES2015, Sass, Android, Java, Git, Sketch, Figma'
+        skills: 'Angular, TypeScript, ES2015, Sass, Android, Java, Git, Sketch'
     },
     {
         company: 'Kik Interactive Inc',
@@ -41,4 +41,61 @@ export const EXPERIENCES: Experience[] = [
         ],
         skills: 'JIRA, Mixpanel, Fabric, Augmentum, Git, Android, Espresso, Java'
     },
+    {
+        company: 'Palette',
+        company_site: 'https://www.kik.com/',
+        logo_link: '/assets/logos/palette-logo.png',
+        job_title: 'Software Developer',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+        mollit anim id est laborum.`,
+        start: 'May 2017',
+        end: 'August 2017',
+        picture_links: [
+            '/assets/palette/1.jpg',
+            '/assets/palette/2.jpg',
+            '/assets/palette/3.jpg',
+        ],
+        skills: 'JIRA, Mixpanel, Fabric, Augmentum, Git, Android, Espresso, Java'
+    },
+    {
+        company: 'Logics Academy',
+        company_site: 'https://www.kik.com/',
+        logo_link: '/assets/logos/logics-logo.png',
+        job_title: 'Research and Development Intern',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+        mollit anim id est laborum.`,
+        start: 'May 2017',
+        end: 'August 2017',
+        picture_links: [
+            '/assets/logics/1.jpg',
+            '/assets/logics/2.jpg',
+            '/assets/logics/3.jpg',
+        ],
+        skills: 'JIRA, Mixpanel, Fabric, Augmentum, Git, Android, Espresso, Java'
+    },
+    {
+        company: 'CityZeen',
+        company_site: 'https://www.kik.com/',
+        logo_link: '/assets/logos/cityzeen-logo.png',
+        job_title: 'Software Engineer',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+        mollit anim id est laborum.`,
+        start: 'May 2017',
+        end: 'August 2017',
+        picture_links: [
+        ],
+        skills: 'JIRA, Mixpanel, Fabric, Augmentum, Git, Android, Espresso, Java'
+    }
 ];
